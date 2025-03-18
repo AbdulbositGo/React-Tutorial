60797e0d9496bc18aaf2bba19e699993ae12043b
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2025-03-18 23:59:55 +0500

    refactor: remove unused assets and styles, implement Card component for layout

---
+++ 02-react-card/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+    return (
+        <div className="card">
+            <img src="https://avatars.githubusercontent.com/u/101085020?v=4" alt="profile" />
+            <h2>Abdulbosit</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam nam minus omnis unde sint quaerat, consequatur error veniam ipsam voluptatem aperiam ratione! Culpa, nostrum. Magnam officiis voluptates facilis sequi doloribus!</p>
+        </div>
+    )
+}
+
+export default Card
