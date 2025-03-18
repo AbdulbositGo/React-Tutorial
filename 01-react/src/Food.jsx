42fa918dc71b60bfc42176e6397624ac0fb58ef1
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2025-03-18 23:28:24 +0500

    First lesson of react on Bro code

---
+++ 01-react/src/Food.jsx
@@ -0,0 +1,20 @@
+
+function Food() {
+    const food1 = "Pizza";
+    const food2 = "Burger";
+    const food3 = "Pasta";
+    const food4 = "Sushi";
+    const food5 = "Tacos";
+
+    return (
+        <ul>
+            <li>{ food1 }</li>
+            <li>{ food2 }</li>
+            <li>{ food3 }</li>
+            <li>{ food4 }</li>
+            <li>{ food5 }</li>
+        </ul>
+    )
+}
+
+export default Food
