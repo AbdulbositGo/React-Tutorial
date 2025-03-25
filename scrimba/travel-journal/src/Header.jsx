60a9c978f473945f4237906cb1f938d31f7605d4
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2025-03-25 22:11:32 +0500

    initialize travel journal project with Vite and React, add ESLint configuration, and create basic components

---
+++ scrimba/travel-journal/src/Header.jsx
@@ -0,0 +1,9 @@
+
+export function Header(){
+    return (
+        <header>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f4/White_Globe_Icon.png" alt="globe image" />
+            <h1>My travel journal.</h1>
+        </header>
+    )
+}
