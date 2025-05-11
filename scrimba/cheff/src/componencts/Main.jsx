479615c45b55225ea8710c0cf8863b8b65cbb60a
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2025-05-11 13:57:00 +0500

    fix: handle ingredient submission and form error state

--- scrimba/cheff/src/componencts/Main.jsx
+++ scrimba/cheff/src/componencts/Main.jsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 export function Main() {
 
     const [ingredients, setIngredients] = useState([])
+    const [formError, setFormError] = useState(false)
     const ingredientsListItems = ingredients.map(
         ingredient =>
         (
@@ -11,20 +12,20 @@ export function Main() {
         )
     )
 
-    function hendleSubmit(event) {
-        event.preventDefault()
-        const formEl = event.currentTarget
-        const formData = new FormData(formEl)
+    function addIngedient(formData) {
         const ingredient = formData.get('ingredient')
-
-        setIngredients([...ingredients, ingredient])
-        formEl.reset()
+        if (ingredient.length > 0) {
+            setIngredients([...ingredients, ingredient])
+        }
+        else {
+            setFormError(true)
+        }
     }
 
     return (
-        <main className="max-w-xl m-auto mt-16 space-y-10">
-            <form method="post" onSubmit={hendleSubmit} className="flex items-center justify-center gap-4">
-                <input type="text" name="ingredient" className="w-full border border-gray-300 shadow-inner rounded-md py-1.5 px-2" placeholder="e.g. oreagano" />
+        <main className="max-w-xl m-auto mt-16 space-y-10 overflow-hidden">
+            <form action={addIngedient} className="flex items-center justify-center gap-4">
+                <input type="text" name="ingredient" className={`w-full border ${formError ? 'border-red-300 focus:outline-red-500' : 'border-gray-300'} shadow-inner rounded-md py-1.5 px-2`} placeholder="e.g. oreagano" />
                 <button type="submit" className="w-52 bg-black text-white rounded-md text-sm py-2">
                     + Add ingredients
                 </button>
